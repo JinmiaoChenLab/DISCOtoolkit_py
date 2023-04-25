7f3f5df5b44b07c3a5b65c0c7f7fd56028617ac6
uddamvathanak <vathanakuddam@gmail.com>
2023-04-25 13:46:20 +0800

    adding doc and commenting to GetMetadata.py

--- discotoolkit/GlobalVariable.py
+++ discotoolkit/GlobalVariable.py
@@ -14,13 +14,9 @@ import logging
 # logger.setLevel(0)
 logging.basicConfig(level = logging.INFO)
 
-# testing for import
-# dummy = random.randint(0,100)
-
 # Define package-level variable
 response = requests.get("http://www.immunesinglecell.org/api/vishuo/getToolkitUrl")
 
-# response = requests.get(disco_url)
 if response.status_code == 200:
     prefix_disco_url = json.loads(response.text)["url"]
 else:
